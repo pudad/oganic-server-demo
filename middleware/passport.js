50a72c40f5c923b894f90deb0267da39342bd3f7
pudad64 <pudad64@gmail.com>
2021-02-16 07:33:04 +0700

    first commit

--- middleware/passport.js
+++ middleware/passport.js
@@ -1,5 +1,5 @@
 const passport = require('passport');
-const Users = require('../models/users.model');
+const Users = require('../model/users.model');
 const configs = require('../configs/main');
 
 var JwtStrategy = require('passport-jwt').Strategy,
@@ -11,12 +11,12 @@ opts.secretOrKey = configs.SECRET_KEY;
 // opts.audience = 'yoursite.net';
 passport.use(new JwtStrategy(opts, async function(jwt_payload, done) {
     try {
-        const haveUser = await Users.findOne({ email: jwt_payload.email});
+        const haveUser = await Users.findOne({ email: jwt_payload.email });
         if (!haveUser) return done(new Error("ไม่พบผู้ใช้ในระบบ"), null);
         if (haveUser) return done(null, { id: haveUser._id, email: haveUser.email, isAdmin: haveUser.isAdmin });
-      } catch (error) {
-          done(error);
-      }
+    } catch (error) {
+        done(error);
+    }
 }));
 
 module.exports.checkLogin = passport.authenticate('jwt', { session: false });
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { loadAllUsers, createUser } = require("../controller/users.controller");
+const { uploadImageProfile } = require("../middleware/multer");
+const { checkDataSignUp } = require("../middleware/userValidator");
 
 /* GET users listing. */
 router.get("/", loadAllUsers);
-router.post("/", createUser);
+router.post("/", [checkDataSignUp, uploadImageProfile.single("imageFile")], createUser);
 
 module.exports = router;
