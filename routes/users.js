77b8e7239a9dc5febaca47178f25e6a2df33d548
pudad64 <pudad64@gmail.com>
2021-02-16 07:49:00 +0700

    first commit

--- routes/users.js
+++ routes/users.js
@@ -3,9 +3,16 @@ const router = express.Router();
 const { loadAllUsers, createUser } = require("../controller/users.controller");
 const { uploadImageProfile } = require("../middleware/multer");
 const { checkDataSignUp } = require("../middleware/userValidator");
+const { checkLogin } = require("../middleware/passport");
+const { isAdmin } = require("../middleware/isAdmin");
 
 /* GET users listing. */
-router.get("/", loadAllUsers);
-router.post("/", [checkDataSignUp, uploadImageProfile.single("imageFile")], createUser);
+// router.get("/", loadAllUsers);
+// router.post("/", [checkDataSignUp, uploadImageProfile.single("imageFile")], createUser);
+
+router
+    .route("/")
+    .post([checkDataSignUp, uploadImageProfile.single("imageFile")], users.createUser)
+    .get([checkLogin, isAdmin], users.loadAllUsers);
 
 module.exports = router;
